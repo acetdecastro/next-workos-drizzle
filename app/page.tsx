d27d0d0135092a64d8b2b609b4dab10718831bba
Ace De Castro <acetdecastro@gmail.com>
2024-07-11 04:50:07 +0800

    Add authentication page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
 import {
   getSignInUrl,
   getSignUpUrl,
   getUser,
-  signOut,
 } from "@workos-inc/authkit-nextjs";
+import { redirect } from "next/navigation";
 
-export default async function LandingPage() {
-  // Retrieves the user from the session or returns `null` if no user is signed in
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+
+export default async function AuthenticationPage() {
   const { user } = await getUser();
 
   // Get the URL to redirect the user to AuthKit to sign in
@@ -16,32 +26,77 @@ export default async function LandingPage() {
   // Get the URL to redirect the user to AuthKit to sign up
   const signUpUrl = await getSignUpUrl();
 
-  /**
-   * If a signed-in user is mandatory, you can use the `ensureSignedIn`
-   * configuration option. If logged out, the below will immediately redirect
-   * the user to AuthKit. After signing in, the user will automatically
-   * be redirected back to this page.
-   * */
-  // const { user } = await getUser({ ensureSignedIn: true });
-
-  https: if (!user) {
+  if (!user) {
     return (
       <>
-        <Link href={signInUrl}>Sign in</Link>;
-        <Link href={signUpUrl}>Sign up</Link>;
+        {/* <div className="md:hidden">
+        <Image
+          src="/examples/authentication-light.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/authentication-dark.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="hidden dark:block"
+        />
+      </div> */}
+        <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+          <div className="relative hidden h-full flex-col bg-muted p-10 text-primary dark:border-r lg:flex">
+            <div className="absolute inset-0 bg-gradient-to-r from-zinc-200 to-sky-200" />
+            <div className="relative z-20 flex items-center text-lg font-medium">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-6 w-6"
+              >
+                <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+              </svg>
+              Acme Inc
+            </div>
+            {/* <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div> */}
+          </div>
+          <div className="lg:p-8">
+            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+              {/* {!user ? (<>
+            <Link href={signInUrl}>
+              <Button className="font-medium">Log In</Button>
+            </Link>
+            <Link href={signUpUrl}>
+              <Button className="font-medium">Sign Up</Button>
+            </Link>
+            </>) : (<div>{user.</div>) } */}
+
+              <Link href={signInUrl}>
+                <Button className="font-medium">Log In</Button>
+              </Link>
+              <Link href={signUpUrl}>
+                <Button className="font-medium">Sign Up</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
 
-  return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut();
-      }}
-    >
-      <p>Welcome back{user.firstName && `, ${user.firstName}`}</p>
-      <button type="submit">Sign out</button>
-    </form>
-  );
+  redirect("/app/dashboard");
 }
